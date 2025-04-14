74ffec5019ac5e74d67a352bc84e84136d41f413
Parker <crookparker99@gmail.com>
2025-04-14 16:43:55 -0400

    Randomize questions every time

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import question_model
 import data
 import quiz_brain
+import requests
+
+API_URL = "https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean"
+
+try:
+    response =  requests.get(API_URL)
+    response.raise_for_status()
+    data.question_data = response.json()
+except requests.exceptions.RequestException as e:
+    print(f"API request failed {e}")
 
 question_bank = []
-for q in data.question_data:
+for q in data.question_data["results"]:
     question_bank.append(question_model.Question(q["question"], q["correct_answer"]))
 
 quiz_giver = quiz_brain.QuizBrain(question_bank)
