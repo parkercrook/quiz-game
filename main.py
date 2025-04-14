73e356a84ec090974e479f5ee73af59af04178a1
Parker <crookparker99@gmail.com>
2025-04-14 16:19:22 -0400

    Finish main quix logic

---
+++ main.py
@@ -0,0 +1,15 @@
+import question_model
+import data
+import quiz_brain
+
+question_bank = []
+for q in data.question_data:
+    question_bank.append(question_model.Question(q["text"], q["answer"]))
+
+quiz_giver = quiz_brain.QuizBrain(question_bank)
+
+while quiz_giver.still_has_questions():
+    quiz_giver.next_question()
+
+print("You've completed the quiz.")
+print(f"Final score: {quiz_giver.score}/{quiz_giver.question_number}")
